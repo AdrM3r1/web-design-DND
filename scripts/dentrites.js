010b9056f4ad34862a9091572fcc122ed241862c
M3r1 <45106091+AdrM3r1@users.noreply.github.com>
2023-10-06 23:22:46 +0200

    arreglos y demas cambios

--- scripts/dentrites.js
+++ scripts/dentrites.js
@@ -1,31 +1,41 @@
-function teleport() {
-	window.location.href = "../html/principal.html";
-}
-
 function cambioLogo() {
 	var nav = document.getElementById('img_logo');
 	var img = document.createElement('img');
-	if(window.screen.width > 720){
+	if (window.screen.width > 720) {
 		img.src = "../images/logo/beholder" + Math.round((Math.random() * 6) + 1) + ".png";
 		nav.appendChild(img);
 	}
 }
 
-function getCookie(name) {
-	var dc = document.cookie;
-	var prefix = name + "=";
-	var begin = dc.indexOf("; " + prefix);
-	if (begin == -1) {
-		begin = dc.indexOf(prefix);
-		if (begin != 0) return null;
-	}
-	else {
-		begin += 2;
-		var end = document.cookie.indexOf(";", begin);
-		if (end == -1) {
-			end = dc.length;
-		}
+function ajaxCall() {
+	let userName = document.getElementById("userName").value;
+	let userEmail = document.getElementById("userEmail").value;
+	let userPassword = document.getElementById("userPassword").value;
+	let userData = { 'nick': userName, 'email': userEmail, 'password': userPassword }
+
+	if (userName || userEmail || userPassword == '') {
+		Swal.fire({
+			position: 'top-end',
+			icon: 'error',
+			title: 'rellene todos los campos',
+			showConfirmButton: false,
+			timer: 1200
+		})
 	}
 
-	return decodeURI(dc.substring(begin + prefix.length, end));
-} 
+	$.ajax({
+		type: 'POST',
+		dataType: 'json',
+		url: 'newUserRegistered',
+		data: userData,
+		success: function (msg) {
+			if (msg) {
+				alert("Somebody" + userName + " was added in list !");
+				location.reload(true);
+			} else {
+				alert("Cannot add to list !");
+			}
+		},
+		data: userData
+	});
+}
