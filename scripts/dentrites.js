4556cf5be0a80821658cbcae78b22f7cd943d00e
M3r1 <45106091+AdrM3r1@users.noreply.github.com>
2023-10-13 22:33:11 +0200

    muchos cambios a mejor

--- scripts/dentrites.js
+++ scripts/dentrites.js
@@ -12,8 +12,7 @@ function ajaxCall() {
 	let userEmail = document.getElementById("userEmail").value;
 	let userPassword = document.getElementById("userPassword").value;
 	let userData = { 'nick': userName, 'email': userEmail, 'password': userPassword }
-
-	if (userName || userEmail || userPassword == '') {
+	/*if (userName == "" || userEmail == "" || userPassword == "") {
 		Swal.fire({
 			position: 'top-end',
 			icon: 'error',
@@ -21,12 +20,12 @@ function ajaxCall() {
 			showConfirmButton: false,
 			timer: 1200
 		})
-	}
-
+	}else{
+	*/
 	$.ajax({
 		type: 'POST',
 		dataType: 'json',
-		url: 'newUserRegistered',
+		url: '',
 		data: userData,
 		success: function (msg) {
 			if (msg) {
@@ -38,4 +37,41 @@ function ajaxCall() {
 		},
 		data: userData
 	});
+}
+
+
+function createCookie(name, value, days) {
+	var name = 'registered';
+	var value = true;
+	var days = 2;
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie() {
+	var c_name = 'registered';
+	var c_value = true;
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+			if(c_value == true){
+				document.getElementById('reg').hidden = true
+			}
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
 }
